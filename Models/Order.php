c45f2b41e892da59e8bf170c2f48e07c20c0ab15
Fabio Bily <fabio.bily93@gmail.com>
2019-06-16 21:01:22 +0200

    fehler suche

--- Models/Order.php
+++ Models/Order.php
@@ -5,7 +5,7 @@ class Order
     public $id;
     public $total_price;
     public $delivery_address_id;
-    public $invoice_address_id;
+    public $payment_id;
     public $user_id;
     public $crdate;
     public $products;
@@ -16,7 +16,7 @@ class Order
         $this->id = $dbResult['id'];
         $this->total_price = $dbResult['total_price'];
         $this->delivery_address_id = $dbResult['delivery_address_id'];
-        $this->invoice_address_id = $dbResult['invoice_address_id'];
+        $this->payment_id = $dbResult['payment_id'];
         $this->user_id = $dbResult['user_id'];
         $this->crdate = $dbResult['crdate'];
         $this->products = $dbResult['products'];
@@ -30,20 +30,20 @@ class Order
         $db = new DB();
 
         if (isset($this->id) && !empty($this->id)) {
-            $db->query("UPDATE orders SET total_price=?, delivery_address_id=?, invoice_address_id=?, user_id=?, products=?, status=? WHERE id = ?", [
+            $db->query("UPDATE orders SET total_price=?, delivery_address_id=?, payment_id=?, user_id=?, products=?, status=? WHERE id = ?", [
                 'd:total_price' => $this->total_price,
                 'i:delivery_address_id' => $this->delivery_address_id,
-                'i:invoice_address_id' => $this->invoice_address_id,
+                'i:payment_id' => $this->payment_id,
                 'i:user_id' => $this->user_id,
                 's:products' => $this->products,
                 's:status' => $this->status,
                 'i:id' => $this->id
             ]);
         } else {
-            $db->query("INSERT INTO orders SET total_price=?, delivery_address_id=?, invoice_address_id=?, user_id=?, products=?, status=?", [
+            $db->query("INSERT INTO orders SET total_price=?, delivery_address_id=?, payment_id=?, user_id=?, products=?, status=?", [
                 'd:total_price' => $this->total_price,
                 'i:delivery_address_id' => $this->delivery_address_id,
-                'i:invoice_address_id' => $this->invoice_address_id,
+                'i:payment_id' => $this->payment_id,
                 'i:user_id' => $this->user_id,
                 's:products' => $this->products,
                 's:status' => $this->status
